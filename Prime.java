6d9a711253db2e9d2f535b47f8dd12e0b9d99381
sushmitha <sushmithachitneni@gmail.com>
2024-12-10 22:34:39 +0530

    thitd

--- Prime.java
+++ Prime.java
@@ -1,7 +1,7 @@
 public class Prime{
     public static void main(String[] args) {
         System.out.println("Prime numbers from 1 to 100 are:");
-        for (int num = 5; num <= 100; num++) {
+        for (int num = 6; num <= 100; num++) {
             boolean isPrime = true;
             for (int divisor = 2; divisor <= num / 2; divisor++) {
                 if (num % divisor == 0){
